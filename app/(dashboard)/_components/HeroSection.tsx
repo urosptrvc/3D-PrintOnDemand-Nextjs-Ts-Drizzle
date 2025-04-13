d7df334e2807b757e38bbd3f58152be950c51f52
Uros Petrovic <132246925+urosptrvc@users.noreply.github.com>
2025-04-13 04:01:22 +0200

    Printer Animation ThreeJs

--- app/(dashboard)/_components/HeroSection.tsx
+++ app/(dashboard)/_components/HeroSection.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
-import { Terminal } from "@/app/(dashboard)/terminal";
+import { PrinterAnimation } from "@/app/(dashboard)/printer-animation";
 
 export function HeroSection() {
   return (
-    <section className="py-20">
+    <section className="pt-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:grid lg:grid-cols-12 lg:gap-8">
           <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
@@ -32,8 +32,10 @@ export function HeroSection() {
               </Link>
             </div>
           </div>
-          <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
-            <Terminal />
+          <div className="mt-16 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+            <div className="w-full h-[600px]">
+              <PrinterAnimation />
+            </div>
           </div>
         </div>
       </div>
