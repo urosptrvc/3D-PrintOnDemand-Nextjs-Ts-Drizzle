ade8f0c5194046096d93286c2b0acef4fda4885b
Uros Petrovic <132246925+urosptrvc@users.noreply.github.com>
2025-04-13 04:05:58 +0200

    Update HeroSection.tsx

--- app/(dashboard)/_components/HeroSection.tsx
+++ app/(dashboard)/_components/HeroSection.tsx
@@ -8,7 +8,7 @@ export function HeroSection() {
     <section className="pt-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:grid lg:grid-cols-12 lg:gap-8">
-          <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+          <div className="pt-16 sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
             <h1 className="text-4xl font-bold text-gray-900 tracking-tight sm:text-5xl md:text-6xl">
               Your 3D Designs
               <span className="block text-orange-500">
